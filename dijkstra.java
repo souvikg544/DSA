e2e3af9b9d092daa55bf2cdb505f1b6d4071f201
Soham Dey <71217733+Arko0809@users.noreply.github.com>
2022-10-25 00:32:01 +0530

    Create dijkstra.java

---
+++ dijkstra.java
@@ -0,0 +1,20 @@
+public class Node {
+    
+    private String name;
+    
+    private List<Node> shortestPath = new LinkedList<>();
+    
+    private Integer distance = Integer.MAX_VALUE;
+    
+    Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+    public void addDestination(Node destination, int distance) {
+        adjacentNodes.put(destination, distance);
+    }
+ 
+    public Node(String name) {
+        this.name = name;
+    }
+    
+    // getters and setters
+}
