66af34b196327fe324de9b80a16e4039fb8a3109
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-02-16 17:44:51 +0530

    Update updated_Iterator.cpp

--- stl/updated_Iterator.cpp
+++ stl/updated_Iterator.cpp
@@ -31,4 +31,27 @@ cout<<(*it)<<endl;
 }
 }
 
-// 3 wa...
+// 3 way.........................
+int main()
+{
+  vector<int> v={2,3,4,5,6,7};
+for(int i=0;i<v.size();++i)
+{
+cout<<v[i]<<"";
+}
+cout<<endl;
+vector<pair<int,int> >v_p={{1,2},{2,3},{3,4}};
+vector<pair<int,int> >:: iterator it;
+for(it=v_p.begin();it!=v_p.end();++it)
+{
+cout<<(*it).first<<" "<<(*it).second<<endl;
+}
+// or in case of pair we use another iterator
+for(it=v_p.begin();it!=v_p.end();++it)
+{
+cout<<(it->first)<<" "<<(it->second)<<endl;
+}
+// note->both are equal
+// (*it).first<=>(it->first)
+
+
