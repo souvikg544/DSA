3a36870cb75e3e046fa380725e461248c98f4241
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-02-17 14:13:46 +0530

    Update Updated_Set.cpp

--- stl/Updated_Set.cpp
+++ stl/Updated_Set.cpp
@@ -39,6 +39,7 @@ print(s);
 }
 
 
+// unorered set is used where order not matter, and chec for present in set
 
 // Question:..Given N strings , print unique strings
 // in lexiographical order 
@@ -81,6 +82,77 @@ s.erase(it);
 }
 e.erase("bcd");
 print(s);
+}
+
+
+// ques: given n strings and q queries ,
+// in each query u are a given string 
+// print yes if string is present else print no
+// n<=10^6 , s<=100 , q<=10^6
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  unordered_set<string>s;
+int n;
+cin>>n;
+for(int i=0;i<n;i++)
+{
+string str;
+cin>>str;
+  s.insert(str);
+}
+int q;
+cin>>q;
+while(q--)
+{
+string str;
+cin>>str;
+if(s.find(str)==s.end())
+{
+cout<<"no\n";
+}
+else{
+cout<<"yes\n";
+}
+}
+}
+
+
+
+
+// Multiset.....allow to insert multiple values in a set..........
+void print(multiset<string> &s)
+{
+for(string value:s)
+{
+cout<<value<<endl;
+}
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  multiset<string>s;
+  s.insert("abc");  //logn
+  s.insert("zsdf");
+s.insert("bcd");
+  s.insert("abc");
+
+
+  // use iterator->it remove only that value present not all but erase remove allrepeated values
+  auto it=s.find("abc");
+  if(it!=s.end())
+  {
+    s.erase(it);
+  }
+  // 2 way.........................
+  s.erase("abc");
+print(s);
+  // mostly all op ->size inser clear find tae logn tc
+  
 }
 
 
